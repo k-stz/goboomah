2ea2429a7b654149a3c1d6371be9b96524640412
Kasjan Chilarski <keistzen@gmail.com>
2025-02-15 22:57:36 +0100

    add processEnemyExplosion, add enemy animations

--- systems/enemy.go
+++ systems/enemy.go
@@ -1,18 +1,65 @@
 package systems
 
 import (
+	"fmt"
+	"image/color"
 	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/k-stz/goboomah/components"
 	"github.com/k-stz/goboomah/tags"
+	"github.com/yohamta/donburi"
 	"github.com/yohamta/donburi/ecs"
 )
+
+// Remove Bomb from ecs and its object from the Collisoin spacce
+func RemoveEnemy(enemyEntry *donburi.Entry, ecs *ecs.ECS) {
+	space := GetSpace(ecs)
+	boundingCircle := components.ShapeCircle.Get(enemyEntry).Circle
+	space.Remove(boundingCircle)
+	ecs.World.Remove(enemyEntry.Entity())
+}
+
+// This function is called when the player was caught in an
+// explosion
+func processEnemyExplosion(enemyEntry *donburi.Entry, ecs *ecs.ECS) {
+	currentTicks := GetTickCount(ecs)
+	state := components.Explodable.Get(enemyEntry)
+	circleShape := components.ShapeCircle.Get(enemyEntry)
+	enemySprite := components.Sprite.Get(enemyEntry)
+	// Enemy exploding state: set
+	if !state.Despawn && state.Exploding && state.ExplodingTick <= currentTicks {
+		state.Exploding = false
+		fmt.Println("I'm despawning", currentTicks, "despawn=", state.Despawn)
+		state.DespawnTick = currentTicks + (60 * 2)
+		state.Despawn = true
+	}
+	// Enemy hurt state
+	if state.Exploding && state.ExplodingTick > currentTicks {
+		// Play damage animation
+		circleShape.Rotation -= 0.1
+		enemySprite.Image.ColorModel().Convert(color.RGBA{255, 0, 0, 255})
+		fmt.Println("Enemy: Im exploding!")
+		fmt.Println("I'm exploding", currentTicks, "despawn=", state.Despawn)
+	}
+	//Enemy dying state
+	if state.Despawn {
+		circleShape.Rotation += 0.1
+	}
+	//Enemy dead state, despawn for good
+	if state.Despawn && state.DespawnTick <= currentTicks {
+		fmt.Println("removing enemy", currentTicks)
+		RemoveEnemy(enemyEntry, ecs)
+	}
+
+}
 
 // Here handle player input and update velocity/movement
 // laeter collision response will be collected from here
 func UpdateEnemy(ecs *ecs.ECS) {
-
+	for enemyEntry := range tags.Enemy.Iter(ecs.World) {
+		processEnemyExplosion(enemyEntry, ecs)
+	}
 	// //tileSpiralEffect(ecs)
 	// enemyEntry, _ := tags.Enemy.First(ecs.World)
 	// enemyShape := components.ShapeCircle.Get(enemyEntry)
@@ -55,6 +102,7 @@ func DrawEnemy(ecs *ecs.ECS, screen *ebiten.Image) {
 	for entry := range tags.Enemy.Iter(ecs.World) {
 		//o := dresolv.GetObject(e)
 		enemySprite := components.Sprite.Get(entry)
+		state := components.Explodable.Get(entry)
 
 		halfW := float64(enemySprite.Image.Bounds().Dx() / 2)
 		halfH := float64(enemySprite.Image.Bounds().Dy() / 2)
@@ -74,13 +122,26 @@ func DrawEnemy(ecs *ecs.ECS, screen *ebiten.Image) {
 		var offsetY float64 = pos.Y //- halfH + halfW
 
 		op := &ebiten.DrawImageOptions{}
+
 		// translate to origin, so scaling and rotation work
 		// intuitively
 		ticks := GetTickCount(ecs)
-		scaleX := Oscillator(math.Cos, int(ticks), 4.0, 1.5, 0.6)
-		scaleY := Oscillator(math.Sin, int(ticks), 4.0, 0.6, 1.5)
+		// make them squish not in sync to be more natural?
+		scaleX := Oscillator(math.Cos, int(ticks), 3.0, 1.5, 0.6)
+		scaleY := Oscillator(math.Sin, int(ticks), 3.0, 0.6, 1.5)
+
+		if state.Exploding {
+			op.ColorScale.Scale(float32(scaleX), 2.0, 2.0, float32(scaleY))
+		}
 
 		op.GeoM.Translate(-halfW, -halfH)
+		if state.Despawn {
+			color := float32(Oscillator(math.Cos, int(ticks), 1.0, 0.1, 4.0))
+			op.ColorScale.Scale(color, color, color, 255)
+			circleShape.Scale += 0.01
+			scaleX = circleShape.Scale
+			scaleY = circleShape.Scale
+		}
 		op.GeoM.Scale(scaleX, scaleY)
 		//op.GeoM.Scale(1.0, 1.0) // undo for debugging
 		op.GeoM.Rotate(rotation)
